2cf9f6d574128781ca3dfb832c99efbb89eb5b73
Erick Vinicius <erick_vinicius_leite@epam.com>
2021-04-24 13:03:03 +0200

    Init project

---
+++ src/main/java/br/com/autoparking/AutoparkingApplication.java
@@ -0,0 +1,13 @@
+package br.com.autoparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoparkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/autoparking/AutoparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.autoparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
