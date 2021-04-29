5983159e2cc294f427caf7241e1c16a569079d79
Erick Vinicius <erick_vinicius_leite@epam.com>
2021-04-29 12:06:23 +0200

    Index

--- src/main/java/br/com/autoparking/controller/HomeController.java
+++ src/main/java/br/com/autoparking/controller/HomeController.java
@@ -2,11 +2,12 @@ package br.com.autoparking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/home")
+	@RequestMapping(value = "/home",method = RequestMethod.GET)
 	public String login() {
 		return "index";
 	}
