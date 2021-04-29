124545400e763fd632bb0360ac8d9ddf080f6dba
Erick Vinicius <erick_vinicius_leite@epam.com>
2021-04-29 10:56:39 +0200

    Criacao index

---
+++ src/main/java/br/com/autoparking/controller/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.autoparking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String login() {
+		return "index.html";
+	}
+}
