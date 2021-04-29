fa1a10aba33baeb444f41d4c7b6b9341247ef074
Erick Vinicius <erick_vinicius_leite@epam.com>
2021-04-29 12:02:56 +0200

    Fix

--- src/main/java/br/com/autoparking/controller/HomeController.java
+++ src/main/java/br/com/autoparking/controller/HomeController.java
@@ -2,14 +2,12 @@ package br.com.autoparking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping(value = "/home")
-	public ModelAndView login() {
-		ModelAndView modelAndView = new ModelAndView("index.html");
-		return modelAndView;
+	public String login() {
+		return "index";
 	}
 }
