161abe9e0204cc25133f4cabe94945908130f11d
Erick Vinicius <erick_vinicius_leite@epam.com>
2021-04-29 12:19:35 +0200

    init

--- src/main/java/br/com/autoparking/AutoparkingApplication.java
+++ src/main/java/br/com/autoparking/AutoparkingApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"service","controller"})
 public class AutoparkingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AutoparkingApplication.class, args);
