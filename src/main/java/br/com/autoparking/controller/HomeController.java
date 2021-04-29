d06a6fe64481728edea663d6c6504f7258ed7b07
Erick Vinicius <erick_vinicius_leite@epam.com>
2021-04-29 11:15:49 +0200

    Heroku

--- src/main/java/br/com/autoparking/controller/HomeController.java
+++ src/main/java/br/com/autoparking/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/home")
 	public String login() {
-		return "/index.html";
+		return "index.html";
 	}
 }
