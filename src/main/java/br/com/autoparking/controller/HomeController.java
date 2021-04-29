9f74fddc2775b72ec384f493582c949b3e4ed75a
Erick Vinicius <erick_vinicius_leite@epam.com>
2021-04-29 11:18:55 +0200

    ModelAndView

--- src/main/java/br/com/autoparking/controller/HomeController.java
+++ src/main/java/br/com/autoparking/controller/HomeController.java
@@ -2,12 +2,14 @@ package br.com.autoparking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping(value = "/home")
-	public String login() {
-		return "index.html";
+	public ModelAndView login() {
+		ModelAndView modelAndView = new ModelAndView("index.html");
+		return modelAndView;
 	}
 }
