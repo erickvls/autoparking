7c25f5567fdd95b42b8d0da817055f9149e2bc3c
Erick Vinicius <erick_vinicius_leite@epam.com>
2021-07-03 12:26:34 +0200

    Mostrando saldo atual

--- src/main/resources/static/js/estatistica.js
+++ src/main/resources/static/js/estatistica.js
@@ -1,21 +1,29 @@
  $(document).ready(function() {
-        $.ajax({
-             url : "/admin/estatisticas/saldo",
-             type : 'POST',
-             data : {
-                       data : "30_DIAS"
-              },
-             beforeSend : function(){
+    var url = '/admin/estatisticas/saldo';
+    requisitarAjax(url,'POST','30_DIAS');
 
-             }
-        })
-        .done(function(msg){
-
-            alert(msg);
-        })
-        .fail(function(jqXHR, textStatus, msg){
-             console.log(msg);
-             alert(msg);
-
-        });
+    $('.mudarValor').click(function() {
+       var dias = this.id;
+      requisitarAjax(url,'POST',dias);
+    });
 });
+
+function requisitarAjax(url,metodo,data){
+            $.ajax({
+                 url : url,
+                 type : metodo,
+                 data : {
+                           data : data
+                  },
+                 beforeSend : function(){
+
+                 }
+            })
+            .done(function(msg){
+                $("#valor").html(msg);
+
+            })
+            .fail(function(jqXHR, textStatus, msg){
+                 alert("Falha na requisição");
+            });
+}
