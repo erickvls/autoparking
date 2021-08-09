88c41f5ba3b323045e51e43ad7d5b1c1ec5ead95
Erick Vinicius <erick_vinicius_leite@epam.com>
2021-08-09 16:52:33 +0200

    Remover backslash

--- src/main/java/br/com/autoparking/controller/cliente/PerfilController.java
+++ src/main/java/br/com/autoparking/controller/cliente/PerfilController.java
@@ -11,12 +11,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 
 @Controller
+@RequestMapping(value = "/home")
 public class PerfilController {
 
     @Autowired
--- src/main/java/br/com/autoparking/controller/cliente/VeiculosController.java
+++ src/main/java/br/com/autoparking/controller/cliente/VeiculosController.java
@@ -7,10 +7,7 @@ import br.com.autoparking.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -18,6 +15,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Controller
+@RequestMapping(value = "/home")
 public class VeiculosController {
 
     @Autowired
